340b0066e5008f684be82033402bce761733cc4e
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-02-16 21:25:01 -0800

    added navbar dropdown with links to additional projects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 export const BootstrapNavbar = () => {
   return (
@@ -12,6 +15,23 @@ export const BootstrapNavbar = () => {
             <Nav.Link href="/workDayScheduler">Work Day Scheduler</Nav.Link>
             <Nav.Link href="/weatherForecast">Weather Forecast</Nav.Link>
             <Nav.Link href="/codingQuiz">React Quiz</Nav.Link>
+            <NavDropdown title="Additional Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item href="https://github.com/justbytes/role_master">
+                Manpower Manager
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://github.com/justbytes/CatWorx.BadgeMaker">
+                Badge Maker
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://github.com/justbytes/behind_the_site">
+                ECommerce Backend
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="https://github.com/justbytes/when-you-cant-readme">
+                ReadMe Generator
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Container>
       </Navbar>
