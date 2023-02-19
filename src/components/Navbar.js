ae16beba81410155ecd1d9dd9fc60c62b16c7997
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-02-19 14:49:47 -0800

    updated navbar and styling for workdayscheduler

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,36 +3,43 @@ import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+import "../style/Navbar.css";
+
 export const BootstrapNavbar = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="dark" className="navbar" variant="dark" expand="lg">
         <Container fluid>
           <Navbar.Brand href="/homePage">Combined Projects</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/homePage">Home</Nav.Link>
-            <Nav.Link href="/passwordGenerator">Password Generator</Nav.Link>
-            <Nav.Link href="/workDayScheduler">Work Day Scheduler</Nav.Link>
-            <Nav.Link href="/weatherForecast">Weather Forecast</Nav.Link>
-            <Nav.Link href="/codingQuiz">React Quiz</Nav.Link>
-            <NavDropdown title="Additional Projects" id="basic-nav-dropdown">
-              <NavDropdown.Item href="https://github.com/justbytes/role_master">
-                Manpower Manager
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="https://github.com/justbytes/CatWorx.BadgeMaker">
-                Badge Maker
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="https://github.com/justbytes/behind_the_site">
-                ECommerce Backend
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="https://github.com/justbytes/when-you-cant-readme">
-                ReadMe Generator
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/homePage">Home</Nav.Link>
+              <NavDropdown title="Backend Projects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="https://github.com/justbytes/role_master">
+                  Manpower Manager
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="https://github.com/justbytes/CatWorx.BadgeMaker">
+                  Badge Maker
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="https://github.com/justbytes/behind_the_site">
+                  ECommerce Backend
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="https://github.com/justbytes/when-you-cant-readme">
+                  ReadMe Generator
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav className="ml-auto">
+              <Nav.Link href="/passwordGenerator">Password Generator</Nav.Link>
+              <Nav.Link href="/workDayScheduler">Work Day Scheduler</Nav.Link>
+              <Nav.Link href="/weatherForecast">Weather Forecast</Nav.Link>
+              <Nav.Link href="/codingQuiz">React Quiz</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
