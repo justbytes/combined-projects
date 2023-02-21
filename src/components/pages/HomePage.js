8c8b1f80595c89bfb5c80463d7e994202d7dcd3e
Jesse Marino <100870863+justbytes@users.noreply.github.com>
2023-02-20 21:33:22 -0800

    styled accordion, added background picture, styled navbar

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -32,42 +32,46 @@ export function HomePage() {
           </Card>
         </div>
         <h1 className="project-title">The Projects</h1>
-        <Accordion defaultActiveKey="0">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Password Generator</Accordion.Header>
-            <Accordion.Body>
-              With options to include character length, uppercase and lowercase
-              letters, numbers, and special characters, this tool can generate
-              strong and unique passwords that can slow down even the most
-              determined attackers. While it may not stop Mr. Robot, it can
-              definitely boost your online security.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Workday Scheduler</Accordion.Header>
-            <Accordion.Body>
-              A scheduling app that enables users to plan out their day with
-              ease by saving their schedule to local storage. It offers a
-              user-friendly color-coded system, highlighting the current,
-              upcoming, and past hours for maximum organization.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="2">
-            <Accordion.Header>Weather Forecast</Accordion.Header>
-            <Accordion.Body>
-              User-friendly weather forecast app that provides both current and
-              5-day forecasts. Stay on top of the weather and plan your day with
-              confidence and never be caught off guard by the weather again.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="3">
-            <Accordion.Header>React Quiz</Accordion.Header>
-            <Accordion.Body>
-              A 10 question quiz on the React framework. You'll have 45 seconds
-              to complete the quiz and your score will shown at the end.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <div className="d-flex justify-content-center align-items-center">
+          <Accordion className="projects" defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Password Generator</Accordion.Header>
+              <Accordion.Body>
+                With options to include character length, uppercase and
+                lowercase letters, numbers, and special characters, this tool
+                can generate strong and unique passwords that can slow down even
+                the most determined attackers. While it may not stop Mr. Robot,
+                it can definitely boost your online security.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Workday Scheduler</Accordion.Header>
+              <Accordion.Body>
+                A scheduling app that enables users to plan out their day with
+                ease by saving their schedule to local storage. It offers a
+                user-friendly color-coded system, highlighting the current,
+                upcoming, and past hours for maximum organization.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Weather Forecast</Accordion.Header>
+              <Accordion.Body>
+                User-friendly weather forecast app that provides both current
+                and 5-day forecasts. Stay on top of the weather and plan your
+                day with confidence and never be caught off guard by the weather
+                again.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="3">
+              <Accordion.Header>React Quiz</Accordion.Header>
+              <Accordion.Body>
+                A 10 question quiz on the React framework. You'll have 45
+                seconds to complete the quiz and your score will shown at the
+                end.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
       </Container>
     </>
   );
